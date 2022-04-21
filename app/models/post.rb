eb8d58ee8673ae1f184436e9a90154ddfe5a5b70
TOMOKKU <69345378+TOMOKKU@users.noreply.github.com>
2022-04-21 23:51:47 +0900

    created posts index

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,5 @@
 class Post < ApplicationRecord
-  validates :content, {presence: true, length{maximam: 140}}
+  validates :content, {presence: true, length:{maximum: 140}}
 
   def self.search(keyword)
      where(["content like ?","%#{keyword}%"])
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  get '/' => 'home#top'
+  get '/' => 'posts#index'
   resources :posts
   get 'search/posts' => 'posts#search'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
